66cf00c04f4e444ca9008115aa551d6b7c699bc7
Erkki Pokkinen <erkki@vetokonsultit.fi>
2024-09-20 18:53:32 +0300

    key=name -> key=id

--- src/components/PersonsComponent.jsx
+++ src/components/PersonsComponent.jsx
@@ -2,7 +2,7 @@ const PersonsComponent = ({filterPersons, handleDelete}) => {
   return (
     <>
       {filterPersons.map(p =>
-        <div key={p.name}>
+        <div key={p.id}>
           {p.name} {p.number} <button onClick={() => handleDelete(p.id, p.name)}>Delete</button>
         </div>
       )}
